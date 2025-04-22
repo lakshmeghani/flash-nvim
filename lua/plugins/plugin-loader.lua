85b54358c378f520eb85071984a0aabfc6b04ee2
lakshmeghani <meghani.laksh@gmail.com>
2025-04-22 22:30:15 +0530

    fixed plugin loader directory key

--- lua/plugins/plugin-loader.lua
+++ lua/plugins/plugin-loader.lua
@@ -1,6 +1,6 @@
 return {
   {
-    dir = "~/.config/learningnvim/NeoVim_Plugins/present.nvim/",
+    dir = "~/.config/nvim/NeoVim_Plugins/present.nvim/",
     ft = "markdown",
     config = function()
       -- let the plugin handle the lazy-loading
